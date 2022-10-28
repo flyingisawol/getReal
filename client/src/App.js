17d69712af651228acc6671c9b116fef21aac4f3
pmish <parshwa138@gmail.com>
2022-10-28 17:48:51 +1100

    more react routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
 
+
 import HomeFeed from './components/HomeFeed'
 import WatchList from './components/WatchList'
 import Search from './components/Search'
 import UserProfile from './components/UserProfile'
 import Profile from './components/Profile'
 import Login from './components/Login'
+import Register from './components/Register'
 
 
 
@@ -21,6 +23,7 @@ function App() {
         <Route path='/getreal/search' element={<Search />} />
         <Route path='/getreal/user-profile' element={<UserProfile />} />
         <Route path='/getreal/login' element={<Login />} />
+        <Route path='/getreal/register' element={<Register />} />
       </Routes> 
     </div>
   );
