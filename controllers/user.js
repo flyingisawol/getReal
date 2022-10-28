822d597d76f038ddeb7bb87bf4c31bcb0ef2096d
manojlovic <manojlovic@outlook.com>
2022-10-28 17:51:25 +1100

    endpoint friday/ basic setup

--- controllers/user.js
+++ controllers/user.js
@@ -2,10 +2,26 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
 const Rate = require('../models/rate')
+const upload = require('../middlewares/upload')
 
 
-router.get('/', (req, res) => {
-    res.send('Hello world')
+//homepage 
+
+
+router.get('/api/getreal', async (req, res) => {
+    const users = await User.find()
+    //.sort() how users will appear/ most active?/ most personality matches
+    res.json(users)
+})
+
+
+
+//show route 
+
+router.get('/api/getreal/:id', upload.single('image'), async (req, res) => {
+    let user = await User.findById(req.params.id).populate('author')
+    //AUTHORISATION HERE
+    res.json(user)
 })
 
 
--- models/rate.js
+++ models/rate.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose')
 
 
 const rateSchema = new mongoose.Schema({
+    author: { type: String, required: true },
     comment: String,
-    rating: { type: Number, required: true },
+    rating: { type: Number, min: 1, max: 5, required: true },
 })
 
 const Rate = mongoose.model('Rate', rateSchema)
