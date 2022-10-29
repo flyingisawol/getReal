406f10717d3bd144523c8f11d560fd06c22c78ed
pmish <parshwa138@gmail.com>
2022-10-29 16:20:27 +1100

    updated urls to include api

--- client/src/components/HomeFeed.js
+++ client/src/components/HomeFeed.js
@@ -5,17 +5,17 @@ const HomeFeed = () => {
       <div className="home-page">
         <h1>Homepage</h1>
           <nav>
-            <Link to='/getreal/profile' >Profile</Link>
+            <Link to='/api/getreal/profile' >Profile</Link>
             <br/>
-            <Link to='/getreal/watchlist' >Watchlist</Link>
+            <Link to='/api/getreal/watchlist' >Watchlist</Link>
             <br/>
-            <Link to='/getreal/search' >Search</Link>
+            <Link to='/api/getreal/search' >Search</Link>
             <br/>
-            <Link to='/getreal/user-profile' >User profile</Link>
+            <Link to='/api/getreal/user-profile' >User profile</Link>
             <br/>
-            <Link to='/getreal/login' >Login</Link>
+            <Link to='/api/getreal/login' >Login</Link>
             <br/>
-            <Link to='/getreal/createprofile' >Create profile</Link>
+            <Link to='/api/getreal/createprofile' >Create profile</Link>
           </nav>
       </div>
     )
